64295921814ad3f628cf442bc363f5a16b8cdbb4
VietNg2023 <minhviet.turku@gmail.com>
2025-04-26 23:19:57 +0300

    add bonus

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{   
+    t_list *temp;
+    
+    if (!lst || !new)
+        return;
+    if (*lst == NULL)
+    {
+        *lst = new;
+        new -> next = NULL;
+        return;
+    }
+    temp = *lst;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+    new -> next = NULL;
+    return;
+}
